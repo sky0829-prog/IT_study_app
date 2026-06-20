c148d4ae827bb390dcd84afbaa67eb6b11c59ae7
sky0829-prog <prog0829ram@gmail.com>
2026-06-21 06:14:23 +0900

    APIとの通信ができない状態だったから直した。

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # データ保存（仮）
 todos = []
